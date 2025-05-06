16b99e2f2283c844de9dfd48a0a12eea7b7a4f53
Jenna-Deamer <jennadeamer@gmail.com>
2025-05-06 10:24:59 -0400

    Add content render to JS, Remove hardcoded content from index.html

--- src/index.js
+++ src/index.js
@@ -1,2 +1,17 @@
 import "./styles.css"
-console.log("hello world");
+const content = document.querySelector("#content");
+
+// Home Content
+const homeBanner = document.createElement('div');
+const homeHeading = document.createElement('h1');
+const homeSubHeading = document.createElement('h2');
+
+homeBanner.id = 'home-banner';
+homeHeading.textContent = 'Some Restaurant Name';
+homeSubHeading.textContent = 'Some Super Awesome Restaurant Slogan!';
+
+content.appendChild(homeBanner);
+homeBanner.appendChild(homeHeading);
+homeBanner.append(homeSubHeading);
+
+
